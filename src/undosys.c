5a77f43538296021a0dbe3c7e258d3058590b1ee
nagolove <dekar.own@gmail.com>
2023-07-05 15:48:09 +0300

    Some undo staff

--- src/undosys.c
+++ src/undosys.c
@@ -1,15 +1,13 @@
 #include "undosys.h"
+#include "koh_destral_ecs.h"
 #include "timers.h"
 #include <assert.h>
+#include <stddef.h>
 #include <stdlib.h>
-
-struct UndoState {
-    de_ecs      *r;
-    TimerMan    *tm;
-};
+#include <string.h>
 
 struct UndoSys {
-    struct UndoState  *st_tmp;
+    struct UndoState  st;
     struct UndoState  *states;
     int     num, cap;
     int     i, j, cur;
@@ -18,6 +16,10 @@ struct UndoSys {
 UndoSys *undosys_new(int cap) {
     UndoSys *us = calloc(1, sizeof(UndoSys));
     assert(us);
+    assert(cap > 0);
+    us->cap = cap;
+    us->states = calloc(us->cap, sizeof(us->states[0]));
+    assert(us->states);
     return us;
 }
 
@@ -36,7 +38,43 @@ void undosys_free(UndoSys *us) {
     free(us);
 }
 
-void undosys_push(UndoSys *us, de_ecs *r, TimerMan *tm) {
+void undosys_push(UndoSys *us, struct UndoState st) {
+    /*
+    if (us->states[us->i].r) {
+        de_ecs_destroy(us->states[us->i].r);
+        us->states[us->i].r = NULL;
+    }
+    if (us->states[us->i].tm) {
+        timerman_free(us->states[us->i].tm);
+        us->states[us->i].tm = NULL;
+    }
+    if (us->states[us->i].udata) {
+        if (us->states[us->i].sz) {
+            free(us->states[us->i].udata);
+        }
+        us->states[us->i].udata = NULL;
+        us->states[us->i].sz = 0;
+    }
+
+    us->states[us->i].r = de_ecs_clone(st.r);
+    assert(us->states[us->i].r);
+
+    us->states[us->i].tm = timerman_clone(st.tm);
+    assert(us->states[us->i].tm);
+
+    if (!st.sz) 
+        us->states[us->i].udata = st.udata;
+    else {
+        assert(st.sz > 0);
+        us->states[us->i].udata = malloc(st.sz);
+        memcpy(us->states[us->i].udata, st.udata, st.sz);
+        us->states[us->i].sz = st.sz;
+    }
+    */
+}
+
+struct UndoState *undosys_get(UndoSys *us) {
+    return NULL;
 }
 
 void undosys_prev(UndoSys *us) {
@@ -51,4 +89,5 @@ void undosys_last(UndoSys *us) {
 void undosys_first(UndoSys *us) {
 }
 
-void undosys_window(UndoSys *us);
+void undosys_window(UndoSys *us) {
+}
--- src/undosys.h
+++ src/undosys.h
@@ -2,14 +2,23 @@
 
 #include "koh_destral_ecs.h"
 #include "timers.h"
+#include <stddef.h>
 
 typedef struct UndoSys UndoSys;
 
+struct UndoState {
+    de_ecs      *r;
+    TimerMan    *tm;
+    void        *udata;
+    size_t      sz;
+};
+
 UndoSys *undosys_new(int cap);
 void undosys_free(UndoSys *us);
-void undosys_push(UndoSys *us, de_ecs *r, TimerMan *tm);
+void undosys_push(UndoSys *us, struct UndoState st);
 void undosys_prev(UndoSys *us);
 void undosys_next(UndoSys *us);
 void undosys_last(UndoSys *us);
 void undosys_first(UndoSys *us);
+struct UndoState *undosys_get(UndoSys *us);
 void undosys_window(UndoSys *us);
