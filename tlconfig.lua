f4868c86e139083416cc295307c28b2a7408fb94
nagolove <dekar.own@gmail.com>
2024-09-26 12:17:29 +0300

    more

---
+++ tlconfig.lua
@@ -0,0 +1,18 @@
+-- vim: set colorcolumn=85
+-- vim: fdm=marker
+
+return {
+    --skip_compat53 = true,
+    --gen_target = "5.1",
+    --global_env_def = "love",
+    --source_dir = "src",
+    --build_dir = "app",
+    include_dir = {
+        "assets/store",
+        "assets/store_return",
+        "assets",
+    },
+    gen_compat = "off",
+    --include = include,
+    --exclude = { },
+}
