9e2b5445f5089f853dbc5cb68a04ed74e6fa52b2
nagolove <dekar.own@gmail.com>
2024-09-26 12:17:16 +0300

    generated files

--- bld.lua
+++ bld.lua
@@ -1,7 +1,50 @@
+
+
+
 return {
-    {
-        artifact = '2048',
-        main = "main.c",
-        src = "src",
-    },
+
+
+   {
+
+
+      not_dependencies = {
+
+
+
+         "lfs",
+
+
+
+         "resvg",
+
+         "rlwr",
+
+      },
+
+
+
+      artifact = "2048",
+
+      main = "main.c",
+
+      src = "src",
+
+      exclude = {},
+
+
+
+      common_define = {},
+
+
+      debug_define = {},
+
+
+
+      release_define = {},
+
+
+
+   },
+
+
 }
